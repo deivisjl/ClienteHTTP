ae829fbafa3d4ca5008d32f35ed76b0653462528
deivisjl <deivisjl@gmail.com>
2019-06-28 10:36:53 -0600

    Implementando el metodo decodeResponse

--- app/Services/MarketService.php
+++ app/Services/MarketService.php
@@ -29,10 +29,14 @@ class MarketService
 	}
 	/**
 	 * 	Decode correspondingly the response
+	 *  @param array $response
+	 *  @return stdClass
 	 */
 	public function decodeResponse($response)
 	{
+		$decodedResponse = json_decode($response);
 
+		return $decodedResponse->data ?? $decodedResponse;
 	}
 	/**
 	 *  Resolve if the request to the service failed
