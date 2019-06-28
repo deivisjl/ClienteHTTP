62e5e927d1c4eeb412f113bb261436f4874f82a7
deivisjl <deivisjl@gmail.com>
2019-06-28 09:26:12 -0600

    Agregando la posibilidad de iniciar sesión con la API

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -56,6 +56,7 @@
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Login') }}
                                 </button>
+                                <a href="#" class="btn btn-success">Connect with API</a>
 
                                 @if (Route::has('password.request'))
                                     <a class="btn btn-link" href="{{ route('password.request') }}">
